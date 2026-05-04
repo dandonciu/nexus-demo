36b7e85e70548bdb5848a45f667126919d26af0c
dandonciu <115440690+dandonciu@users.noreply.github.com>
2026-05-04 14:36:20 +0300

    Update app.py

--- app.py
+++ app.py
@@ -232,7 +232,7 @@ if st.session_state.role == "angajat":
                 col_s2.metric("📦 Stoc CUTII libere Disp.", av_box)
                 col_s3.metric("🔄 Cutii per Palet", f"{p_data['conversion']} buc")
                 
-                col_q1, col_q2 = st.columns(2)
+                col_q1, col_q2, col_goala = st.columns([1, 1, 1])
                 with col_q1: order_pal = st.number_input("Nr. PALEȚI comandați:", min_value=0, step=1, key=f'input_pal_{st.session_state.reset_counter}')
                 with col_q2: order_box = st.number_input("Nr. CUTII comandate:", min_value=0, step=1, key=f'input_box_{st.session_state.reset_counter}')
                 
