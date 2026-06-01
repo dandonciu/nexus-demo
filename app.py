7664b1a1340e69430e40c533c67d4ae453762466
dandonciu <115440690+dandonciu@users.noreply.github.com>
2026-06-01 12:14:31 +0300

    Update app.py

--- app.py
+++ app.py
@@ -130,10 +130,12 @@ elif st.session_state.current_module == 'Email':
 elif st.session_state.current_module == 'Receptie':
     st.button("⬅️ Înapoi la Panoul Principal", on_click=go_home)
     st.title("📥 Modul Recepție Marfă (Așteaptă Dezvoltare)")
+    st.info ("📥 Modul Recepție Marfă - -> În Dezvoltare")
 
 elif st.session_state.current_module == 'Transport':
     st.button("⬅️ Înapoi la Panoul Principal", on_click=go_home)
     st.title("🚚 Modul Transport & Curieri (Așteaptă Dezvoltare)")
+    st.info("🚚 Modul Transport - -> În Dezvoltare")
 
 elif st.session_state.current_module == 'SmartBill':
     st.button("⬅️ Înapoi la Panoul Principal", on_click=go_home)
