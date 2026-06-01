a2a0f1704accc73d8874f7e94c843e2726a9e18f
dandonciu <115440690+dandonciu@users.noreply.github.com>
2026-06-01 22:44:54 +0300

    Refactor database initialization comments and structure

--- backend/database/clients_config.py
+++ backend/database/clients_config.py
@@ -18,21 +18,21 @@ client_aliases = {
     "DSCM TECH SRL": {"Role Albe Stergere": "Role autocut albe TAD 220m", "Saci negri mari": "Saci menaj 120L negri LDPE"}
 }
 
-# Inițializare Database Gestiune WMS (Pentru st.session_state)
+# Inițializare Database Gestiune WMS
 def init_db():
     return {
         "Prosoape V verzi": {
-            "cod_nir": "NIR-209520", "oracle_pal": "PAL NV710", "oracle_box": "NV710", 
+            "cod_nir": "NIR-209520", "oracle_pal": "PAL NV710", "oracle_box": "NV710",
             "stock_pal": 4, "stock_box": 15, "conversion": 32, "um_baza": "Pachete", "conversie_baza": 20,
             "livrari_totale": pd.DataFrame({"Client": ["S.C. SIDE GRUP S.R.L."], "Data": ["10-01-2024"], "Volum_Paleti": [2], "Status_Plata": ["Achitat"]})
         },
         "Role autocut albe TAD 220m": {
-            "cod_nir": "NIR-156350", "oracle_pal": "PAL BKTp721", "oracle_box": "BKTp721", 
+            "cod_nir": "NIR-156350", "oracle_pal": "PAL BKTp721", "oracle_box": "BKTp721",
             "stock_pal": 4, "stock_box": 32, "conversion": 48, "um_baza": "Role", "conversie_baza": 6,
             "livrari_totale": pd.DataFrame({"Client": ["DSCM TECH SRL"], "Data": ["15-01-2024"], "Volum_Paleti": [4], "Status_Plata": ["Achitat"]})
         },
         "Saci menaj 120L negri LDPE": {
-            "cod_nir": "NIR-211125", "oracle_pal": "PAL IVFLX120LD-N", "oracle_box": "IVFLX120LD-N", 
+            "cod_nir": "NIR-211125", "oracle_pal": "PAL IVFLX120LD-N", "oracle_box": "IVFLX120LD-N",
             "stock_pal": 5, "stock_box": 20, "conversion": 60, "um_baza": "Role", "conversie_baza": 15,
             "livrari_totale": pd.DataFrame({"Client": ["DSCM TECH SRL"], "Data": ["12-02-2024"], "Volum_Paleti": [1], "Status_Plata": ["În termen"]})
         }
