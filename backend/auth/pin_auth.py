cbf5e51cc2bb36bd31c4034c1ff0be1fb5f02109
dandonciu <115440690+dandonciu@users.noreply.github.com>
2026-06-03 19:35:46 +0300

    Update pin_auth.py

--- backend/auth/pin_auth.py
+++ backend/auth/pin_auth.py
@@ -69,7 +69,7 @@ def verify_2fa(username):
         return False
   #===============================================  
     # Container centrat pentru 2FA
-    col1, col2, col3 = st.columns([1, 1, 1])
+    col1, col2, col3 = st.columns([1, 3, 1])
     with col2:
         st.markdown("---")
         st.markdown("<h3 style='text-align: center;'>🔐 Verificare cod securitate</h3>", unsafe_allow_html=True)
