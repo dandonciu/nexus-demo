4770c46372c0d5d9a57926c6be7a15bd5408aafe
dandonciu <115440690+dandonciu@users.noreply.github.com>
2026-06-05 21:13:33 +0300

    Update vault.py

--- backend/services/vault.py
+++ backend/services/vault.py
@@ -1,14 +1,17 @@
+
+
 import streamlit as st
 
-def render_Vault_module():
-    st.title("🎨 Vault")
-    st.info("Back-up permanent, securizat, satisface Big4.")
+def render_vault_module():
+    st.title("🛡️ Vault")
+    st.markdown("---")
+     st.info("Back-up permanent, securizat, satisface Big4.")
     
-    st.divider()
-    
-    st.success("""
+    if st.button("⬅️ Înapoi la Panoul Principal"):
+        st.session_state.current_module = 'Home'
+           st.success("""
   dB Back-up zilnic
   dB Back-up săptămînal
   dB Out of Office săptămînal
     """)
-
+        st.rerun()
