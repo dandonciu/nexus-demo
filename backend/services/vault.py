010ec698241038858c1d5ee42bfca4957126e298
dandonciu <115440690+dandonciu@users.noreply.github.com>
2026-06-06 11:40:20 +0300

    Update vault.py

--- backend/services/vault.py
+++ backend/services/vault.py
@@ -3,11 +3,12 @@ import streamlit as st
 def render_vault_module():
     st.title("🛡️ Vault")
     st.markdown("---")
-    st.info("🔐 Modul Vault")
-    st.info("dB Back-up - zilnic / dB Back-up - săptămînal / dB Back-up Out of Office - săptămînal).")
+    st.info("🔐 Modul Vault - dB Back-up - zilnic / dB Back-up - săptămînal / dB Back-up Out of Office - săptămînal.")
+    
     st.info("- Conform legislației contabile actuale, termenul de păstrare pentru anumite documente justificative și registre este de 5 ani.")
     st.info("- Legea permite păstrarea în format digital.")
-
+    st.info("- Situații financiare anuale: Se păstrează timp de 10 ani.Statele de plată și dosarele de personal: Se păstrează timp de 50 de ani.")
+    st.info("- Legea permite păstrarea în format digital.")
 
 if st.button("⬅️ Înapoi la Panoul Principal"):
     st.session_state.current_module = 'Home'
