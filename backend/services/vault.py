40c52943defebf2e495f8dc784fcf4863c1ef6e5
dandonciu <115440690+dandonciu@users.noreply.github.com>
2026-06-06 11:49:09 +0300

    Update vault.py

--- backend/services/vault.py
+++ backend/services/vault.py
@@ -9,18 +9,13 @@ def render_vault_module():
 
     
     st.success("""
-    **Exemplu de prompt:**
+    **Legislație:**
     
-    La imaginea atașată te rog să faci următoarele modificări: 
-    - Să ștergi logo-ul de sus: wipe it clean / Xxwoven 
-    - Pentru a echilibra imaginea mută mai sus textul: XTRA PRECISION XP50 și rîndul de sub el
-    - Înlocuiește la ”Packing: 50 sheets” 50 cu 90. Vom avea: ”Packing: 90 sheets”
-    - Înlocuiește culoarea roșie la dunga roșie oblică din stînga-sus cu negru. (Deci negru în loc de roșu)
-    - Înlocuiește culoarea roșie la triunghiul din stînga-jos cu culoare negru. (Deci negru în loc de roșu)
-    - Înlocuiește codul de bare din imagine cu codul de bare din a doua imagine atașată.   
-    
-    *Img1_eticheta.pdf*  
-    *Img2_Cod_de_bare.pdf*
+    - Conform legislației contabile actuale, termenul de păstrare pentru anumite documente justificative și registre este de 5 ani, calculat de la data de 1 iulie a anului următor încheierii exercițiului financiar.
+
+- Situații financiare anuale: Se păstrează timp de 10 ani.Statele de plată și dosarele de personal: Se păstrează timp de 50 de ani
+
+- Legea permite păstrarea în format digital.
     """)
 if st.button("⬅️ Înapoi la Panoul Principal"):
     st.session_state.current_module = 'Home'
