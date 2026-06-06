7cd632aa4370d90d4b5ea96779fa6224cead74c0
dandonciu <115440690+dandonciu@users.noreply.github.com>
2026-06-06 11:44:34 +0300

    Update vault.py

--- backend/services/vault.py
+++ backend/services/vault.py
@@ -5,10 +5,23 @@ def render_vault_module():
     st.markdown("---")
     st.info("🔐 Modul Vault - dB Back-up - zilnic / dB Back-up - săptămînal / dB Back-up Out of Office - săptămînal.")
     
-    st.info("- Conform legislației contabile actuale, termenul de păstrare pentru anumite documente justificative și registre este de 5 ani.
-    - Situații financiare anuale: Se păstrează timp de 10 ani.Statele de plată și dosarele de personal: Se păstrează timp de 50 de ani.")
-    - Legea permite păstrarea în format digital.")
 
+  st.divider()
+    
+    st.success("""
+    **Exemplu de prompt:**
+    
+    La imaginea atașată te rog să faci următoarele modificări: 
+    - Să ștergi logo-ul de sus: wipe it clean / Xxwoven 
+    - Pentru a echilibra imaginea mută mai sus textul: XTRA PRECISION XP50 și rîndul de sub el
+    - Înlocuiește la ”Packing: 50 sheets” 50 cu 90. Vom avea: ”Packing: 90 sheets”
+    - Înlocuiește culoarea roșie la dunga roșie oblică din stînga-sus cu negru. (Deci negru în loc de roșu)
+    - Înlocuiește culoarea roșie la triunghiul din stînga-jos cu culoare negru. (Deci negru în loc de roșu)
+    - Înlocuiește codul de bare din imagine cu codul de bare din a doua imagine atașată.   
+    
+    *Img1_eticheta.pdf*  
+    *Img2_Cod_de_bare.pdf*
+    """)
 if st.button("⬅️ Înapoi la Panoul Principal"):
     st.session_state.current_module = 'Home'
     st.rerun()
